6da95f9f3454f908e50b39196017d91026072201
m25703 <114353660+m25703@users.noreply.github.com>
2023-07-13 08:03:44 +0530

    updated

--- server/models/UserCards.js
+++ server/models/UserCards.js
@@ -1,17 +1,48 @@
 module.exports = (sequelize, DataTypes) => {
-  const Posts = sequelize.define("Posts", {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastClick: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW, 
-    },
-    lastIncrement: {
-      type: DataTypes.FLOAT,
-      defaultValue: 1,
-    },
-  });
-  return Posts;
-};
+    const UserCards = sequelize.define("UserCards", {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      cardId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Posts",
+          key: "id",
+        },
+      },
+      cardData: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      // Additional columns for user-card interactions
+      // e.g., date/time of interaction, specific changes made, etc.
+      interactionDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+  
+    UserCards.associate = (models) => {
+      UserCards.belongsTo(models.Users, {
+        foreignKey: "userId",
+      });
+      UserCards.belongsTo(models.Posts, {
+        foreignKey: "cardId",
+      });
+    };
+  
+    return UserCards;
+  };
+  
